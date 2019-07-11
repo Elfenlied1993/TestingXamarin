60e9cbefe35856b9cfbde991a7c40ac716e4f518
Nikita <elfenlied1990lol@gmail.com>
2019-07-11 11:06:16 +0300

    Simple UIform created

---
+++ UIform/UIform/UIform/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+
+namespace UIform
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SolidColorBrush brush = new SolidColorBrush(Colors.Gray);
+            brush.Opacity = 0.25;
+            Head.Text = "You can";
+            Google.Text = "Sign up with google";
+            Cloud.Text = "and store your data in the cloud*, or";
+            Start.Text = "Just Start";
+            Phone.Text = "and store all your data on the phone**";
+            Privacy.Text =
+                "*Further you can check our Privacy Policy, also you will be able to delete your account at any time";
+            Privacy_2.Text = "**If you delete, reinstall the app all data will be lost";
+        }
+    }
+}
